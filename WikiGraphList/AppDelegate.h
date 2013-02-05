2aa22278e4269fa1276ac95c67469e451a611bb3
csnowleopard <csnowleopard@gmail.com>
2013-02-05 11:01:04 -0500

    Initial Commit

---
+++ WikiGraphList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WikiGraphList
+//
+//  Created by Christopher Sadowski on 2/5/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WikiGraphList/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WikiGraphList
+//
+//  Created by Christopher Sadowski on 2/5/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
